a7eea0e6fc22b6cf835b8b966aa9e7582fc0af17
Swadesh Thapa <88982763+SwadeshnotSwodesh@users.noreply.github.com>
2024-02-12 14:59:16 +0545

    Updated spr.java
    The problem of the computer choosing the same input whenever the user chooses the input repeatedly has been fixed.

--- spr.java
+++ spr.java
@@ -1,3 +1,7 @@
+/*
+
+This program has got some issues because of the Constructor Game(). The problem with this code is that Whenever the same input is repeated over the period of time, the computer chose the same input too
+
 import java.util.Scanner;
 import java.util.Random;
  class Game
@@ -6,7 +10,7 @@ import java.util.Random;
     public int computerInput;
     public int point=0;
     public int computerPoint=0;
-    Game()
+    Game()   //this creates a problem in this code
     {
         Random random=new Random();
         this.computerInput=random.nextInt(2);
@@ -89,4 +93,120 @@ public class spr
         game.comparePoint();
     }
 }
+
+
+*/
+
+
+//the below mentioned code is the corrected version of the code for the Scissor, Paper, and game.
+
+import java.util.Scanner;
+import java.util.Random;
+ class Game
+{
+    public int userInput;
+    public int computerInput;
+    public int point=0;
+    public int computerPoint=0;
+    /*Game()
+    {
+        //Using the random generator in constructor here is creating problem
+        //Whenever the same input is repeated over the period of time, the computer chose the same input too
+        //The problem is occuring because of computerInput is set only once in the game and gets invoked when the object is created.
+        //Hence, the computer's input remains the same for the rest of the round too.
+        Random random=new Random();
+        this.computerInput=random.nextInt(3);
+    }*/
+    public void randomNumberGenerator()
+    {
+        Random random=new Random();
+        this.computerInput=random.nextInt(3);
+    }
+    public void takeUserInput()
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter 0 for Scissor, 1 for Paper, and 2 for Rock first:");
+        this.userInput=scan.nextInt();
+    }
+    public void scoreOutput()
+    {
+        System.out.println("--->"+" Your score: "+point+" <---");
+        System.out.println("--->"+" Computer score: "+computerPoint+" <---");
+    }
+    public void compare()
+    {
+         
+        int option=userInput;
+        if(option==0||option==1||option==2)
+        {
+        if(computerInput==userInput)
+        {
+            System.out.println("--->"+" Same choice!! "+"<---");
+            scoreOutput();
+        }
+        else if(userInput==0&&computerInput==1||userInput==1&&computerInput==2||userInput==2&&computerInput==0)
+        {
+            System.out.println("--->"+" You took one point here!! "+"<---");
+            point++;
+            scoreOutput();
+        }
+        else 
+        {
+            System.out.println("--->"+" Computer took one point here!! "+"<---");
+            computerPoint++;
+            scoreOutput();
+        }
+    }
+    else 
+    {
+        System.out.println("Invalid option...");
+        System.out.println("Restart the game please!!");
+        System.exit(0);
+    }
+        System.out.println("Computer Input: "+computerInput);
+    }
+    public void comparePoint()
+    {
+            if(point==computerPoint)
+        {
+            System.out.println("GAME IS DRAW!!");
+            System.out.println("Your total point is = Computer's total point is = "+point);
+        }
+        else if(point>computerPoint)
+        {
+            System.out.println("YOU HAVE WON THIS MATCH!!");
+            System.out.println("Your total point is: "+point);
+            System.out.println("Computer's total point is: "+computerPoint);
+        }
+        else 
+        {
+            System.out.println("COMPUTER HAS WON THIS MATCH!!");
+            System.out.println("Your total point is: "+point);
+            System.out.println("Computer's total point is: "+computerPoint);
+        }
+    }
+}
+public class spr
+{
+    public static void main(String[] args)
+    {
+        Game game=new Game();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of times you want to run the program first:");
+        int noOfTime=sc.nextInt();
+        for(int i=0;i<noOfTime;i++)
+        {
+        game.randomNumberGenerator();
+        game.takeUserInput();
+        game.compare();
+        }
+        game.comparePoint();
+    }
+}
+
+//Have a fun!
+
+
+
+
 
